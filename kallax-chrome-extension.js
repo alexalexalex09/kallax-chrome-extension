33de97aaec8f11a5ff84be6887b2651e36f6619c
Alex <frogitts@gmail.com>
2022-07-18 08:05:32 -0400

    Scaffolding

--- kallax-chrome-extension.js
+++ kallax-chrome-extension.js
@@ -12,25 +12,33 @@ window.addEventListener("load", function () {
   /***********************/
   /*      For BGG        */
   /***********************/
-  //Get list of all anchor tags
-  console.log("Getting links");
-  var link = [];
-  whiteList.forEach((e) => {
-    link.push(document.querySelectorAll("." + e + " a"));
-  });
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    const tabUrl = tabs[0].url;
+    if (tabUrl.substr(0, 25 == "https://boardgamegeek.com")) {
+      //Get list of all anchor tags
+      console.log("Getting links");
+      var link = [];
+      whiteList.forEach((e) => {
+        link.push(document.querySelectorAll("." + e + " a"));
+      });
 
-  //Filter list to only board game links
-  console.log("Filtering non-boardgames");
-  var boardgames = [];
-  link.forEach((e) => {
-    boardgames.push(fn.filterBGG(e));
-  });
+      //Filter list to only board game links
+      console.log("Filtering non-boardgames");
+      var boardgames = [];
+      link.forEach((e) => {
+        boardgames.push(fn.filterBGG(e));
+      });
 
-  //Add a placeholder icon to all board game links
-  boardgames.forEach((e) => {
-    e.forEach((el) => {
-      fn.addPiece(el);
-    });
+      //Add a placeholder icon to all board game links
+      boardgames.forEach((e) => {
+        e.forEach((el) => {
+          fn.addLogo(el);
+        });
+      });
+    }
+    if (tabUrl.substr(0, 30 == "https://www.boardgameatlas.com")) {
+      //TODO: Add support for BGA
+    }
   });
 });
 
@@ -39,8 +47,32 @@ window.addEventListener("load", function () {
 /***********************/
 
 const fn = {
+  login: function (username, password) {
+    const body = {
+      username: username,
+      password: password,
+    };
+    chrome.storage.sync.get(["jwt"], function (result) {
+      if (typeof result[jwt] == "undefined") {
+        //If we're not already logged in, create a fetch request with username and password
+        const options = {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        fetch("https://kallax.io/login", options) //TODO: Use an actual endpoint
+          .then((response) => response.text())
+          .then((data) => {
+            //When the response is received, store the jwt in chrome sync storage for future use
+            chrome.storage.sync.set({ jwt: data.jwt });
+          });
+      }
+    });
+  },
   filterBGG: function (links) {
-    //filter the list of links to leave only boardgames
+    //filter the list of links on Board Game Geek to leave only boardgames
     var boardgames = [];
     links.forEach((e) => {
       var href = e.getAttribute("href");
@@ -56,11 +88,11 @@ const fn = {
     });
     return boardgames;
   },
-  addPiece: function (e) {
+  addLogo: function (e) {
     //Add the piece after each boardgame found
-    var el = document.createElement("kallaxPiece");
-    el.classList.add("kallaxPiece", "kallaxGrey");
-    fn.getTTSMod(e[2].trim()).then((res) => {
+    var el = document.createElement("kallaxLogo");
+    el.classList.add("kallaxLogo", "kallaxGrey");
+    fn.getKallaxID(e[2].trim()).then((res) => {
       switch (res) {
         case -1:
           break;
@@ -68,97 +100,60 @@ const fn = {
           el.classList.remove("kallaxGrey");
           el.classList.add("kallaxRed");
           break;
-        case 1:
-          el.classList.add("kallaxYellow");
-          break;
         default:
+          el.classList.remove("kallaxGrey");
           el.classList.add("kallaxGreen");
       }
-      this.addLink(el, e[2].trim());
+      this.addMenu(el, e[2].trim());
       e[0].parentNode.insertBefore(el, e[0]);
     });
   },
-  getTTSMod: function (title) {
-    //Get the number of mods found from chrome sync storage if available, scrape Steam otherwise
+  getKallaxID: function (title) {
+    //Get the id from local storage based on title, otherwise search kallax
     console.log("Getting " + title);
     var promise = new Promise(function (resolve, reject) {
       chrome.storage.sync.get([title], (result) => {
         if (
-          //true == true || //Testing
-          typeof result[title] == "undefined" ||
-          Date.now() - result[title].date > 1000 * 60 * 60 * 24 * 7 //Fetch the results again if they're older than 7 days
+          typeof result[title] != "undefined" &&
+          Date.now() - result[title].date < 1000 * 60 * 60 * 24 * 7 //Fetch the results again if they're older than 7 days
         ) {
-          /*//First try to find them in the database
-            Cache.findOne({ title: title }).exec(function (err, cache) {
-              //If a cache was found and it's younger than 7 days, use that
-              if (cache && Date.now() - cache.date > 1000 * 60 * 60 * 24 * 7) {
-                //Update the local database, which was out of date
-                fn.saveLocal(cache.title, cache.count);
-                resolve(cache.count);
-              } else {
-                //Didn't find a cached result: find a new result and update the chrome sync and mongo cache*/
-
-          var fixed =
-            "Fixed the connection string error caused by rotating config vars";
-          fixed.toString();
-          fetch(
-            "https://bgg-tts-cors.herokuapp.com/steamcommunity.com/workshop/browse/?appid=286160&searchtext=" +
-              title
-          )
-            .then((response) => response.text())
-            .then((data) => {
-              data = data.toString();
-
-              //Filter down the HTML to only workshop item titles that match ours
-              var regex = `class="workshopItemTitle.*?>` + title + "(.*?)<";
-              var regexName = new RegExp(regex, "gi");
-              var matches = data.matchAll(regexName);
-              var count = 0;
-
-              //Loop through the matches to filter further
-              for (const match of matches) {
-                if (
-                  match[1].indexOf("[") == -1 ||
-                  //Brackets are OK only if they are for English or to designate that it's scripted
-                  match[1].indexOf("[EN]") != -1 ||
-                  match[1].indexOf("[Script") != -1 ||
-                  match[1].indexOf("[script") != -1
-                ) {
-                  //Success! Increase the count
-                  count++;
-                } else {
-                  console.log("Didn't match: " + match[1]);
-                }
-              }
-              //Store result in Chrome sync
-              fn.saveLocal(title, count);
-              //Store result in Mongo DB
-              /*cache = { title: title, count: count, date: Date.now() };
-                    cache.save();*/
-              resolve(count);
-            });
-          /*}
-            });*/
-        } else {
           resolve(result[title].numMatches);
         }
+        // Didn't find anything locally, so search Kallax
+        const body = {
+          title: title,
+        };
+        const options = {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        fetch("https://kallax.io/getSingleGameFromTitle", options) //TODO: Use an actual endpoint
+          .then((response) => response.text())
+          .then((data) => {
+            data = data.toString();
+            //Store result in Chrome sync
+            fn.saveLocal(title, data.id);
+            resolve(data.id);
+          });
       });
     });
     return promise;
   },
-  addLink: function (el, title) {
-    //Add a link to each piece
+  addMenu: function (el, title) {
+    //Add the Kallax menu
+    //TODO: Create a hidden menu element
+    //TODO: Add menu element as child of passed element
     el.addEventListener("click", () => {
-      window.open(
-        "https://steamcommunity.com/workshop/browse/?appid=286160&searchtext=" +
-          title
-      );
+      //TODO: Toggle hidden state of child
     });
   },
-  saveLocal: function (title, count) {
+  saveLocal: function (title, id) {
     //Cache Steam data locally in Chrome sync storage to avoid scraping when possible
     var toSave = {};
-    toSave[title] = { numMatches: count, date: Date.now() };
+    toSave[title] = { id: id, date: Date.now() };
     chrome.storage.sync.set(toSave, (res) => {});
   },
 };
